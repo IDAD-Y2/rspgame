cacbe809b5f6caef86200a219620e69ea0e3e701
jaySimz99 <58327702+jaySimz99@users.noreply.github.com>
2021-06-23 13:59:14 +0100

    edit 4

--- gameplay.js
+++ gameplay.js
@@ -9,6 +9,9 @@ let speed = 0;
 const SLOW_FRAME = 6;
 var pickCardRand;
 var playerPick;
+var computerPick;
+var cardIsPicked = false;  
+var rounds; 
 
 const rock = new Image();
 rock.src = "rock.png";
@@ -46,19 +49,35 @@ function clicked(event) {
 
     if (x < 160 && x > 32 && y > 110 && y < 292) { //780 = 580+(200) <- image width
         playerPick = "rock";
-        alert(playerPick);
+        cardIsPicked = true;
     }
 
     if (x < 300 && x > 172 && y > 110 && y < 292) { //780 = 580+(200) <- image width
         playerPick = "paper";
-        alert(playerPick);
+        cardIsPicked = true;
     }
 
     if (x < 440 && x > 312 && y > 110 && y < 292) { //780 = 580+(200) <- image width
         playerPick = "scissors";
-        alert(playerPick);
+        cardIsPicked = true;
     }
 }
+
+
+
+    var randCard = Math.floor((Math.random() * 3));
+    
+    if (randCard == 0){
+        computer = "rock";     
+    }
+
+    if (randCard == 1){
+        computer = "paper";     
+    }
+
+    if (randCard == 2){
+        computer = "scissors";     
+    }
 
 
 function animate() {
@@ -72,7 +91,56 @@ function animate() {
         else framex = 0;
     }
     speed++;
+
+    if (cardIsPicked == true) {
+        context.font = "40px Arial";
+        context.fillStyle = "white";
+        context.strokeStyle = "rgba(92, 11, 90, 1)";
+        context.textAlign = "center";
+        context.fillText("You picked " + playerPick, 230, 330);
+        context.strokeText("You picked " + playerPick, 230, 330);
+    }
+
+    if (computer == "rock" && cardIsPicked == true)
+    {context.drawImage(rock2, framex * SPRITE_WDT, 0, 700, 700, 600, 100, 200, 200);}
+
+    if (computer == "paper" && cardIsPicked == true)
+    {context.drawImage(paper2, framex * SPRITE_WDT, 0, 700, 700, 600, 100, 200, 200);}
+
+    if (computer == "scissors" && cardIsPicked == true )
+    {context.drawImage(scissors2, framex * SPRITE_WDT, 0, 700, 700, 600, 100, 200, 200);}
+
+    if (playerPick == "rock" && computer == "scissors" ||
+        playerPick == "scissors" && computer == "paper" ||
+        playerPick == "paper" && computer == "rock")
+    { context.font = "40px Arial";
+    context.fillStyle = "white";
+    context.strokeStyle = "rgba(92, 11, 90, 1)";
+    context.textAlign = "center";
+    context.fillText("You WIN", 400, 370);
+    context.strokeText("You WIN", 400, 370);}
+
+    if (playerPick == "scissors" && computer == "rock" ||
+playerPick == "paper" && computer == "scissors" ||
+playerPick == "rock" && computer == "paper")
+{ 
+context.fillText("You LOSE", 400, 370);
+context.strokeText("You LOSE", 400, 370);
 }
+
+if (playerPick == "scissors" && computer == "scissors" ||
+playerPick == "paper" && computer == "paper" ||
+playerPick == "rock" && computer == "rock")
+{ 
+context.fillText("DRAW", 400, 370);
+context.strokeText("DRAW", 400, 370);
+}
+
+
+
+}
+
+
 
 function gameloop() {
     animate();
